bebef41a8b62f4b89d035f9e8b2c76e9228b9376
Alexander Hvilshøj <alexander@hvilshoej.dk>
2017-09-06 11:18:12 +0200

    initial commit

---
+++ DnD Treasure generator/DnD Treasure generator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD_Treasure_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
